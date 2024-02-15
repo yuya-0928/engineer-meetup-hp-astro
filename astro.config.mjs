ef26a22ae6e83bcad30db41668728e71db31c851
yuya-0928 <hirako.yuuya0928.4620@gmail.com>
2024-02-16 00:25:29 +0900

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightBlog from 'starlight-blog'
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlightBlog(),
+		starlight({
+			title: 'エンジニア集会',
+			social: {
+				discord: 'https://discord.gg/zaTjFtDRP7',
+				'x.com': 'https://twitter.com/VRENGAssoc',
+				youtube: 'https://www.youtube.com/channel/UC_6lKeNBPKkTBPXtlV5n7Cg',
+			},
+			sidebar: [
+				{
+					label: '初めての方へ',
+					items: [
+						{ label: 'エンジニア集会とは？', link: '/community/what_is_engineer-meetup/' },
+						{ label: 'Code of Conduct', link: '/community/code_of_conduct/' },
+						{ label: '開催しているイベント', link: '/community/events/' },
+						{ label: 'エンジニア集会に関する記事', link: '/community/public_print/' },
+					],
+				},
+			],
+		}),
+	],
+});
---
+++ src/content/docs/community/public_print.md
@@ -0,0 +1,9 @@
+---
+title: エンジニア集会に関する記事
+description: エンジニア集会に関する記事
+---
+
+<iframe class="note-embed" src="https://note.com/embed/notes/n43aaa44e2420" style="border: 0; display: block; max-width: 99%; width: 494px; padding: 0px; margin: 10px 0px; position: static; visibility: visible;" height="400"></iframe><script async src="https://note.com/scripts/embed.js" charset="utf-8"></script>
+
+https://www.moguravr.com/vrchat-study-meeting-and-business-networking-meetings-featured-events/
+
